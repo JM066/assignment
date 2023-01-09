b3d501e45510e91bc4579b987c48b1bfdb328ac7
minalee <mina.lee066@gmail.com>
2023-01-09 11:39:26 +0700

    Testing in progress

--- src/view/hook/useFetchUsers/test/useFetchUsers.test.tsx
+++ src/view/hook/useFetchUsers/test/useFetchUsers.test.tsx
@@ -1,28 +1,52 @@
-import { renderHook, waitFor } from '@testing-library/react'
+import { renderHook, act, waitFor } from '@testing-library/react'
 import useFetchUsers from '../useFetchUsers'
-import mockAxios from 'jest-mock-axios'
+
 jest.mock('axios')
 
-describe('fetchData', () => {
-    afterEach(() => {
-        mockAxios.reset()
+describe('fetch users', () => {
+    it('Start fetch users', async () => {
+        // const mockFetch = jest.fn(() => Promise.resolve({ data: {} }))
+        const mockFetch = jest.fn()
+        mockFetch('https://jsonplaceholder.typicode.com/users')
+
+        await act(async () => renderHook(() => useFetchUsers('users')))
+        expect(mockFetch).toBeCalledWith(
+            'https://jsonplaceholder.typicode.com/users'
+        )
+        expect(mockFetch).toBeCalled()
+        expect(mockFetch).toBeCalledTimes(1)
     })
 
-    it('fetches users successfully', async () => {
-        const { result } = renderHook(() => useFetchUsers('uers'))
-        expect(result.current.loading).toBeTruthy()
+    it('while fetching data', async () => {
+        jest.fn(
+            () => new Promise((resolve, reject) => console.log(resolve, reject))
+        )
+        const { result } = renderHook(() => useFetchUsers('users'))
+        expect(result.current.loading).toBe(true)
+        const resolve = jest.fn(() => Promise.resolve())
+        resolve()
         await waitFor(() => {
-            expect(result.current.loading).toBeFalsy()
+            expect(result.current.loading).toBe(false)
         })
     })
-
-    it('fetch fails', async () => {
+    it('fetch success', async () => {
+        const mockFetch = jest.fn(() => Promise.resolve({ data: {} }))
+        mockFetch()
         const { result } = renderHook(() => useFetchUsers('users'))
-        const message = 'Network Error'
-        mockAxios.get.mockRejectedValueOnce(new Error(message))
         await waitFor(() => {
-            expect(result.current.data).toEqual(undefined)
-            expect(result.current.loading).toBeTruthy()
+            expect(result.current.loading).toBe(false)
+            expect(mockFetch.mockResolvedValue({ data: {} }))
+            expect(result.current.error).toBe(null)
+        })
+    })
+    it('fetch fails', async () => {
+        const message = 'Network Error'
+        const mockFetch = jest.fn(() => Promise.reject(new Error(message)))
+        const { result } = renderHook(() => useFetchUsers('users'))
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false)
+            expect(mockFetch.mockRejectedValue(message))
+            expect(result.current.data).toBe(undefined)
         })
     })
 })
